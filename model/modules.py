d4fa45752e79039b1119f1fdab48f9c7367e4d80
Qiucheng Miao <miaoqiucheng@outlook.com>
2024-08-13 17:43:14 +0800

    Update modules.py

--- model/modules.py
+++ model/modules.py
@@ -271,38 +271,6 @@ class ConvLayer(nn.Module):
 
 Tensor = torch.Tensor
 def gumbel_softmax(logits: Tensor, tau: float = 1, hard: bool = False, eps: float = 1e-10, dim: int = -1) -> Tensor:
-    r"""
-    Samples from the Gumbel-Softmax distribution (`Link 1`_  `Link 2`_) and optionally discretizes.
-    Args:
-      logits: `[..., num_features]` unnormalized log probabilities
-      tau: non-negative scalar temperature
-      hard: if ``True``, the returned samples will be discretized as one-hot vectors,
-            but will be differentiated as if it is the soft sample in autograd
-      dim (int): A dimension along which softmax will be computed. Default: -1.
-    Returns:
-      Sampled tensor of same shape as `logits` from the Gumbel-Softmax distribution.
-      If ``hard=True``, the returned samples will be one-hot, otherwise they will
-      be probability distributions that sum to 1 across `dim`.
-    .. note::
-      This function is here for legacy reasons, may be removed from nn.Functional in the future.
-    .. note::
-      The main trick for `hard` is to do  `y_hard - y_soft.detach() + y_soft`
-      It achieves two things:
-      - makes the output value exactly one-hot
-      (since we add then subtract y_soft value)
-      - makes the gradient equal to y_soft gradient
-      (since we strip all other gradients)
-    Examples::
-        >>> logits = torch.randn(20, 32)
-        >>> # Sample soft categorical using reparametrization trick:
-        >>> F.gumbel_softmax(logits, tau=1, hard=False)
-        >>> # Sample hard categorical using "Straight-through" trick:
-        >>> F.gumbel_softmax(logits, tau=1, hard=True)
-    .. _Link 1:
-        https://arxiv.org/abs/1611.00712
-    .. _Link 2:
-        https://arxiv.org/abs/1611.01144
-    """
     if eps != 1e-10:
         warnings.warn("`eps` parameter is deprecated and has no effect.")
 
